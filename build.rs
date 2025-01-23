0b26e837885a7c2432d3b408367380a98ddc469e
hill <hill@hilll.dev>
2025-01-23 23:18:31 +0000

    do build script

--- build.rs
+++ build.rs
@@ -4,7 +4,31 @@ use std::process::Command;
 fn main() {
     let script_path = PathBuf::from("./generate_tables.py");
 
+    //create venv
     let status = Command::new("python")
+        .arg("-m")
+        .arg("venv")
+        .arg("venv")
+        .status()
+        .expect("Failed to create venv");
+
+    if !status.success() {
+        panic!("Failed to create venv");
+    }
+
+    // call pip
+    let status = Command::new("venv/bin/pip")
+        .arg("install")
+        .arg("-r")
+        .arg("music21/requirements.txt")
+        .status()
+        .expect("Failed to install requirements");
+
+    if !status.success() {
+        panic!("Failed to install requirements");
+    }
+
+    let status = Command::new("venv/bin/python")
         .arg(&script_path)
         .status()
         .expect("Failed to execute Python script");
