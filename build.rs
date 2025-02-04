b665c1114b9338406faa88d21ec84a668808e9b3
hill <hill@hilll.dev>
2025-02-04 01:04:16 +0000

    todays work

--- build.rs
+++ build.rs
@@ -26,12 +26,23 @@ enum Cardinality {
 }
 
 fn main() -> PyResult<()> {
-    let script_path = PathBuf::from("./generate_tables.sh");
+    // let script_path = PathBuf::from("./generate_tables.sh");
 
-    let status = Command::new("bash")
-        .arg(script_path.clone())
+    // let status = Command::new("bash")
+    //     .arg(script_path.clone())
+    //     .status()
+    //     .expect("Failed to execute command");
+
+    // if !status.success() {
+    //     panic!("script exited with status: {}", status);
+    // };
+
+    let status = Command::new("python3.12")
+        .arg("pip")
+        .arg("install")
+        .arg("music21")
         .status()
-        .expect("Failed to execute command");
+        .expect("failed");
 
     if !status.success() {
         panic!("script exited with status: {}", status);
@@ -39,16 +50,17 @@ fn main() -> PyResult<()> {
 
     Python::with_gil(|py| {
         let sys = py.import("sys")?;
-        let path = sys.getattr("path")?;
-        path.call_method1("append", ("./venv/lib/python3.12/site-packages",))?;
-        path.call_method1("append", ("./music21",))?;
+        // let path = sys.getattr("path")?;
+        // path.call_method1("append", ("./venv/lib/python3.12/site-packages",))?;
+        // path.call_method1("append", ("./music21",))?;
 
-        let chord = PyModule::import(py, "music21.chord")?;
+        let chord = py.import("music21.chord")?;
         let tables = chord.getattr("tables")?;
         println!("tables: {:?}", tables);
 
         println!("cargo:rerun-if-changed={}", script_path.display());
-        println!("cargo:rerun-if-changed=music21/music21/chord/tables.py");
+        println!("cargo:rerun-if-changed=./music21/music21/chord/tables.py");
+        println!("cargo:rerun-if-changed={}", "./build.rs");
         Ok(())
     })
 }
