f0fb423a535cdb48090297edce16ce753d47ceb4
hill <hill@hilll.dev>
2026-04-27 22:23:21 +0200

    format with alejandra

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix flake for music21-rs development";
 
   inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
