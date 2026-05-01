739bcf12d834a7ffdf6a347f3fab1f6e64e220da
hill <hill@hilll.dev>
2026-05-01 03:08:36 +0200

    Update polyrhythm sound example for rodio 0.22

--- examples/polyrhythmsound.rs
+++ examples/polyrhythmsound.rs
@@ -1,6 +1,6 @@
 use music21_rs::polyrhythm::Polyrhythm;
 use rodio::{
-    OutputStreamBuilder, Sink,
+    DeviceSinkBuilder,
     source::{SineWave, Source},
 };
 use std::{thread::sleep, time::Duration};
@@ -9,17 +9,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut poly = Polyrhythm::from_time_signature(4, 120, &[6, 8])?;
     let tick_duration = Duration::from_secs_f32(poly.tick_duration()? as f32);
 
-    let stream_handle = OutputStreamBuilder::open_default_stream()?;
+    let sink_handle = DeviceSinkBuilder::open_default_sink()?;
     loop {
         let (tick, triggers) = poly.next().unwrap();
         println!("Tick: {tick} - Triggers: {triggers:?}");
         for (i, &trigger) in triggers.iter().enumerate() {
             if trigger {
                 let freq = 100.0 * (i + 1) as f32;
-                let sink = Sink::connect_new(stream_handle.mixer());
                 let beep = SineWave::new(freq).take_duration(Duration::from_millis(100));
-                sink.append(beep);
-                sink.detach();
+                sink_handle.mixer().add(beep);
             }
         }
         sleep(tick_duration);
